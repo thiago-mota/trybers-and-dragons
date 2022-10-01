97a80c523c99bad49bef7baf0bed01ed69a6a74b
Thiago Mota <th.araujomota@gmail.com>
2022-10-01 16:57:45 -0300

    feat: adds Halfing race into race index

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,6 +1,7 @@
 import Race from './Race';
 import Dwarf from './Dwarf';
 import Elf from './Elf';
+import Halfing from './Halfing';
 
 export default Race;
-export { Dwarf, Elf };
+export { Dwarf, Elf, Halfing };
