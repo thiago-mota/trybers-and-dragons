cf74a7bcefb9fb2faf0db9667126c96e7f69e9dc
Thiago Mota <th.araujomota@gmail.com>
2022-10-01 16:04:19 -0300

    feat: creates createdRacesInstances method

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -14,6 +14,10 @@ abstract class Race {
   get dexterity(): number {
     return this._dexterity;
   }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented.');
+  }
 }
 
 export default Race;
