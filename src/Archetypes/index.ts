ba42e57da73ef9daeb7dc1fb0cd120d407e0e8c5
Thiago Mota <th.araujomota@gmail.com>
2022-10-03 14:31:11 -0300

    feat: adds ranger on Archetype index

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,6 +1,7 @@
 import Archetype from './Archetype';
 import Mage from './Mage';
 import Necromancer from './Necromancer';
+import Ranger from './Ranger';
 
 export default Archetype;
-export { Mage, Necromancer };
+export { Mage, Necromancer, Ranger };
