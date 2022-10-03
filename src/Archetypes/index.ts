a89a234c0cb885a3e05b4e1513cc30a9f42163d6
Thiago Mota <th.araujomota@gmail.com>
2022-10-03 14:34:55 -0300

    feat: adds warriors on Archetype index

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -2,6 +2,7 @@ import Archetype from './Archetype';
 import Mage from './Mage';
 import Necromancer from './Necromancer';
 import Ranger from './Ranger';
+import Warrior from './Warrior';
 
 export default Archetype;
-export { Mage, Necromancer, Ranger };
+export { Mage, Necromancer, Ranger, Warrior };
